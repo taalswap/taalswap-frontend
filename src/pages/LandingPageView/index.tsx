c1587c441022dc6ada7db4e2b832fb7608eda90b
helloljho <helloljho@gmail.com>
2021-06-03 15:41:59 +0900

    Using Topbar from taalswap-uikit

--- src/pages/LandingPageView/index.tsx
+++ src/pages/LandingPageView/index.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
-import TopBar from './TopBar';
-import SectionTop from './Section_Top';
-import SectionBottom from './Section_Bottom';
-import Footer from './Footer';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+// import TopBar from './TopBar'
+import useAuth from 'hooks/useAuth'
+import { useWeb3React } from '@web3-react/core'
+
+import { TopBar } from 'taalswap-uikit'
+import SectionTop from './Section_Top'
+import SectionBottom from './Section_Bottom'
+import Footer from './Footer'
+import './App.css'
 
 const AppWrapper = styled.div`
   height: '100%';
   width: '100%';
-`;
+`
 
 const LandingPageView = () => {
-  return (
-    <div className='wrap'>
-        <TopBar />
-        <SectionTop />
-        <SectionBottom />
-        <Footer />
-    </div>
-  );
-};
+  const { account } = useWeb3React()
+  const { login, logout } = useAuth()
 
-export default LandingPageView;
+  return (
+    <div className="wrap">
+      <TopBar account={account} login={login} logout={logout} />
+      <SectionTop />
+      <SectionBottom />
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPageView
