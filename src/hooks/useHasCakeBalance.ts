eb0294c679e4487e25be3c58687e82bad9699a70
Peter Nahm <hwnahm@gmail.com>
2021-06-04 18:39:43 +0900

    Keep following latest 40 commits

--- src/hooks/useHasCakeBalance.ts
+++ src/hooks/useHasCakeBalance.ts
@@ -6,7 +6,7 @@ import useTokenBalance from './useTokenBalance'
  * A hook to check if a wallet's TAL balance is at least the amount passed in
  */
 const useHasCakeBalance = (minimumBalance: BigNumber) => {
-  const cakeBalance = useTokenBalance(getTaalAddress())
+  const { balance: cakeBalance } = useTokenBalance(getTaalAddress())
   return cakeBalance.gte(minimumBalance)
 }
 
