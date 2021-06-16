69cd5ab78fcf54732cb50357e4b9933c69b6d734
hanabpg3 <84052764+hanabpg3@users.noreply.github.com>
2021-06-16 19:00:13 +0900

    Landing page modify

--- src/pages/LandingPageView/TopBar.tsx
+++ src/pages/LandingPageView/TopBar.tsx
@@ -18,32 +18,32 @@ const TopBar = () => {
       </div>
       <div className="top_menu">
         <div>
-          <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="/" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px" }}>
             Home
           </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="http://localhost:3000/#/swap" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="http://localhost:3000/#/swap" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px" }}>
             Swap
           </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="http://localhost:3000/#/pool" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="http://localhost:3000/#/pool" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px" }}>
             Liquidity
           </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="/farms" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="/farms" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px" }}>
             Farms
           </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="/pools" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="/pools" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px"}}>
             Staking
           </Link>
         </div>
         <div style={{ marginLeft: '30px' }}>
-          <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
+          <Link href="/" style={{ color: '#fff', textDecoration: 'none',fontSize:"14px" }}>
             IDO
           </Link>
         </div>
