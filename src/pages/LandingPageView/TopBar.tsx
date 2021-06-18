93ceabef61a75390e0d1dd1c3323ed00a0254a33
helloljho <helloljho@gmail.com>
2021-06-18 15:23:40 +0900

    change image

--- src/pages/LandingPageView/TopBar.tsx
+++ src/pages/LandingPageView/TopBar.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { Target } from 'react-feather'
 import { Link } from 'taalswap-uikit'
@@ -8,62 +8,62 @@ import logo_imgChange from './images/TAAL_Logo.svg'
 
 const TopBar = () => {
   const { account } = useWeb3React()
-  const [scrollPosition, setScrollPosition] = useState(0);
-    const updateScroll = () => {
-        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
-    }
-    useEffect(()=>{
-        window.addEventListener('scroll', updateScroll);
-    });
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop)
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll)
+  })
   console.log(account)
   return (
-    <div className={scrollPosition < 50 ? "original_header" : "change_header"}>
-    <div className="tabbar_wrap">
-      <div>
-        <Link href="/">
-          <img src={logo_img} alt="logo_img" className="top_logo" />
-          <img src={logo_imgChange} alt="logo_img" className="top_logochange" />
-        </Link>
-      </div>
-      <div className="top_menu">
+    <div className={scrollPosition < 50 ? 'original_header' : 'change_header'}>
+      <div className="tabbar_wrap">
         <div>
-          <Link href="/" style={{color:'#00ab55',textDecoration: 'none',fontSize:"14px" }}>
-            Home
+          <Link href="/">
+            <img src={logo_img} alt="logo_img" className="top_logo" />
+            <img src={logo_imgChange} alt="logo_img" className="top_logochange" />
           </Link>
         </div>
-        <div style={{ marginLeft: '30px' }}>
-          <Link href="http://localhost:3000/#/swap" style={{textDecoration: 'none',fontSize:"14px" }}>
-            Swap
-          </Link>
+        <div className="top_menu">
+          <div>
+            <Link href="/" style={{ color: '#00ab55', textDecoration: 'none', fontSize: '14px' }}>
+              Home
+            </Link>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+            <Link href="http://localhost:3000/#/swap" style={{ textDecoration: 'none', fontSize: '14px' }}>
+              Swap
+            </Link>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+            <Link href="http://localhost:3000/#/pool" style={{ textDecoration: 'none', fontSize: '14px' }}>
+              Liquidity
+            </Link>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+            <Link href="/farms" style={{ textDecoration: 'none', fontSize: '14px' }}>
+              Farms
+            </Link>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+            <Link href="/pools" style={{ textDecoration: 'none', fontSize: '14px' }}>
+              Staking
+            </Link>
+          </div>
+          <div style={{ marginLeft: '30px' }}>
+            <Link href="/" style={{ textDecoration: 'none', fontSize: '14px' }}>
+              IDO
+            </Link>
+          </div>
+          <div>
+            <input type="button" value="Connect Wallet" className="connect_btn" />
+          </div>
         </div>
-        <div style={{ marginLeft: '30px' }}>
-          <Link href="http://localhost:3000/#/pool" style={{textDecoration: 'none',fontSize:"14px" }}>
-            Liquidity
-          </Link>
-        </div>
-        <div style={{ marginLeft: '30px' }}>
-          <Link href="/farms" style={{textDecoration: 'none',fontSize:"14px" }}>
-            Farms
-          </Link>
-        </div>
-        <div style={{ marginLeft: '30px' }}>
-          <Link href="/pools" style={{textDecoration: 'none',fontSize:"14px"}}>
-            Staking
-          </Link>
-        </div>
-        <div style={{ marginLeft: '30px' }}>
-          <Link href="/" style={{textDecoration: 'none',fontSize:"14px" }}>
-            IDO
-          </Link>
-        </div>
-        <div>
-          <input type="button" value="Connect Wallet" className="connect_btn" />
+        <div className="mobile_menu" style={{ cursor: 'pointer' }}>
+          <Topmenu />
         </div>
       </div>
-      <div className="mobile_menu" style={{ cursor: 'pointer' }}>
-        <Topmenu />
-      </div>
-    </div>
     </div>
   )
 }
