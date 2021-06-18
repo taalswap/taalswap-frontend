bb97411b9195395b1ef8818dfd647930b8d7f5e5
helloljho <helloljho@gmail.com>
2021-06-18 15:23:40 +0900

    change image

--- src/pages/LandingPageView/topmenu.tsx
+++ src/pages/LandingPageView/topmenu.tsx
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
-import { Target } from 'react-feather';
-import { Link } from 'taalswap-uikit';
+import React, { useState } from 'react'
+import { Target } from 'react-feather'
+import { Link } from 'taalswap-uikit'
 
 const Search = () => {
-  const [show, toggleShow] = React.useState(true);
+  const [show, toggleShow] = React.useState(true)
   return (
     <div>
-      <input type="button" className='mobilenav_icon'
-        onClick={() => toggleShow(!show)}
-      />{ show ? null : <Results /> } 
+      <input type="button" className="mobilenav_icon" onClick={() => toggleShow(!show)} />
+      {show ? null : <Results />}
       {show && Results}
     </div>
   )
-  }
-  
-  const Results = () => (
-    <div className='hide_menu' id='results'>
-    <span className='arrow_box'>-</span>
-    <ul>
-      <li><Link href='/' style={{color:'#00ab55',textDecoration:'none'}}><span className='home_icon'>home_icon</span>Home</Link></li>
-      <li><Link href='#/swap' style={{textDecoration:'none'}}><span className='swap_icon'>swap_icon</span>Swap</Link></li>
-      <li><Link href='#/pool' style={{textDecoration:'none'}}><span className='liquidity_icon'>liquidity_icon</span>Liquidity</Link></li>
-      <li><Link href='http://localhost:3001/farms' style={{textDecoration:'none'}}><span className='farms_icon'>Farms_icon</span>Farms</Link></li>
-      <li><Link href='/' style={{textDecoration:'none'}}><span className='connect_icon'>connect_icon</span>Coneect Wallet</Link></li>
-    </ul>
-    </div>
-  )
+}
 
-  export default Search;
+const Results = () => (
+  <div className="hide_menu" id="results">
+    <span className="arrow_box">-</span>
+    <ul>
+      <li>
+        <Link href="/" style={{ color: '#00ab55', textDecoration: 'none' }}>
+          <span className="home_icon">home_icon</span>Home
+        </Link>
+      </li>
+      <li>
+        <Link href="#/swap" style={{ textDecoration: 'none' }}>
+          <span className="swap_icon">swap_icon</span>Swap
+        </Link>
+      </li>
+      <li>
+        <Link href="#/pool" style={{ textDecoration: 'none' }}>
+          <span className="liquidity_icon">liquidity_icon</span>Liquidity
+        </Link>
+      </li>
+      <li>
+        <Link href="http://localhost:3001/farms" style={{ textDecoration: 'none' }}>
+          <span className="farms_icon">Farms_icon</span>Farms
+        </Link>
+      </li>
+      <li>
+        <Link href="/" style={{ textDecoration: 'none' }}>
+          <span className="connect_icon">connect_icon</span>Coneect Wallet
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Search
