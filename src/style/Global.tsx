79158998312e5f80b59632e22c87de74e0770c8c
hanabpg3 <84052764+hanabpg3@users.noreply.github.com>
2021-06-21 11:45:59 +0900

    font modify

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 // eslint-disable-next-line import/no-unresolved
 import { PancakeTheme } from 'taalswap-uikit/dist/theme'
+import './app.css'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
