3ddb997cbb0756260065a0c0ac76f9f7d63d250e
seongjin1026 <seongjin.kim@owdin.network>
2021-06-29 18:54:25 +0900

    Change to share languages and themes across domains

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,9 +1,15 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { ThemeContext as StyledThemeContext } from 'styled-components'
 import { ThemeContext } from 'contexts/ThemeContext'
 
 const useTheme = () => {
   const { isDark, toggleTheme } = useContext(ThemeContext)
+
+  useEffect(() => {
+    const postMsg = {'key': 'IS_DARK', 'value': JSON.stringify(isDark)}
+    // @ts-ignore
+    postCrossDomainMessage(postMsg)
+  }, [isDark])
   const theme = useContext(StyledThemeContext)
   return { isDark, toggleTheme, theme }
 }
