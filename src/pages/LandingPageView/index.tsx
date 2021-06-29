b0ed8d08ae6b5c9d278967b7c00b2d56a6fbd507
helloljho <helloljho@gmail.com>
2021-06-29 13:13:30 +0900

    landing page - surport multi lang, dark mode

--- src/pages/LandingPageView/index.tsx
+++ src/pages/LandingPageView/index.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 // import TopBar from './TopBar'
 import useAuth from 'hooks/useAuth'
 import { useWeb3React } from '@web3-react/core'
-
+import { languageList } from 'config/localization/languages'
+import { useTranslation } from 'contexts/Localization'
+import useTheme from 'hooks/useTheme'
 import { TopBar } from 'taalswap-uikit'
 import SectionTop from './Section_Top'
 import SectionBottom from './Section_Bottom'
@@ -18,10 +20,20 @@ const AppWrapper = styled.div`
 const LandingPageView = () => {
   const { account } = useWeb3React()
   const { login, logout } = useAuth()
-
+  const { isDark, toggleTheme } = useTheme()
+  const { currentLanguage, setLanguage, t } = useTranslation()
   return (
     <div className="wrap">
-      <TopBar account={account} login={login} logout={logout} />
+      <TopBar
+        account={account}
+        login={login}
+        logout={logout}
+        langs={languageList}
+        setLang={setLanguage}
+        currentLang={currentLanguage.code}
+        isDark={isDark}
+        toggleTheme={toggleTheme}
+      />
       <SectionTop />
       <SectionBottom />
       <Footer />
