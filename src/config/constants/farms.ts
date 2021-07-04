76ade697f1425d1c3de14045c5b58df9a7ff5dfa
Peter Nahm <hwnahm@gmail.com>
2021-07-05 02:24:45 +0900

    chore: taalswap deployed again on ropsten for test

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -9,8 +9,8 @@ const farms: FarmConfig[] = [
     pid: 0,
     lpSymbol: 'TAL',
     lpAddresses: {
-      3: '0x78a24accf5f557b004c1313b2cad2e85c345d971',
-      4: '0xe18e460d38441027b6672363d68c9088f3d773bf',
+      3: '0x1e94361329257647669dde7dc8c869624aa424ea',
+      4: '0xe18e460d38441027b6672363d68c9088f3d773bf',    // TaalToken
       97: '0xb01258e6fb20bde11dab2b65b2ffa197d725c91c',
       56: '',
     },
@@ -21,7 +21,7 @@ const farms: FarmConfig[] = [
     pid: 1,
     lpSymbol: 'TAL-ETH LP',
     lpAddresses: {
-      3: '0x0f8c6983fed6617dc72874b17c4849e765bafc23',
+      3: '0x8ecebf4b71f6143e57c2144a273b023e8b647882',
       4: '0x98d2c51626bdac2a257dda01a60deb51cee2cee6',
       97: '0x74f8ee76936280e744c663b1d17b68e58a34cc29',
       56: '',
@@ -31,46 +31,34 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 2,
-    lpSymbol: 'USDT-ETH LP',    // <-- USDC-ETH 로 변경
+    lpSymbol: 'USDC-ETH LP',
     lpAddresses: {
-      3: '0xd3ebd345775b0491e8842fdbad71256f5b15bf8a',
+      3: '0x61abc4ad785ee3952d220e2ba8a8fa26e83d4e05',
       4: '0x73b1501e0ab88dc81d95f8ed9aa67938afabda4b',
       97: '0xd2dafced17fc1679fbabfd02d10b001405dcd299',
       56: '',
     },
-    token: tokens.usdt,         // <-- usdc 로 변경
+    token: tokens.usdc,
     quoteToken: tokens.weth,
   },
   {
     pid: 3,
-    lpSymbol: 'ETH-USDC LP',    // <-- ETH-USDT 로 변경
+    lpSymbol: 'USDT-ETH LP',
     lpAddresses: {
-      3: '0x1e5aefcbf561cade978cf7c87762e97e772090c5',
+      3: '0x0ebd0874149762bc2f3f2d1113446ed93f47d40c',
       4: '0x1a7bab68f22eb8f84e8920b120bec8130efd6db8',
       97: '',
       56: '',
     },
-    token: tokens.weth,
-    quoteToken: tokens.usdc,    // <-- usdt 로 변경
+    token: tokens.usdt,
+    quoteToken: tokens.weth,
   },
   {
     pid: 4,
-    lpSymbol: 'TAL-USDT LP',
-    lpAddresses: {
-      3: '0x4eeb7094e34c8272d6c3d813bb7d18ced7874ab9',
-      4: '0x036126bda6190605fadd2028caa8dd7be13c045a',
-      97: '',
-      56: '',
-    },
-    token: tokens.taal,
-    quoteToken: tokens.usdt,
-  },
-  {
-    pid: 5,
     lpSymbol: 'TAL-USDC LP',
     lpAddresses: {
-      3: '0x43dee61dec9abdbfe4a8a1f38182f7c4d2931228',
-      4: '0xa17d74b8ae149f08c8a1db09e4a55e158163fed1',
+      3: '0xd4d839ba02e7f1c131f38c51d04404ef0cc08282',
+      4: '0x036126bda6190605fadd2028caa8dd7be13c045a',
       97: '',
       56: '',
     },
@@ -78,16 +66,28 @@ const farms: FarmConfig[] = [
     quoteToken: tokens.usdc,
   },
   {
-    pid: 6,
-    lpSymbol: 'USDC-USDT LP',
+    pid: 5,
+    lpSymbol: 'TAL-USDT LP',
     lpAddresses: {
-      3: '0xda51cc4b05bbec589ce7114061f4346bfc1c2a07',
+      3: '0x9117ae3e02b4c2e6d37faa6dd8fecb4b2ca49ef9',
+      4: '0xa17d74b8ae149f08c8a1db09e4a55e158163fed1',
+      97: '',
+      56: '',
+    },
+    token: tokens.taal,
+    quoteToken: tokens.usdt,
+  },
+  {
+    pid: 6,
+    lpSymbol: 'USDT-USDC LP',
+    lpAddresses: {
+      3: '0x25ea31604eace1b21c0c578fedb25de320d350de',
       4: '0xbac68c775b94b2999e8e2fdcda3ea5e57bbb9176',
       97: '',
       56: '',
     },
-    token: tokens.usdc,
-    quoteToken: tokens.usdt,
+    token: tokens.usdt,
+    quoteToken: tokens.usdc,
   },
   // // {
   // //   pid: 13,
