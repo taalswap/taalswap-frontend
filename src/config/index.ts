4097acb2a2f8007ade8eab4dcd349a87a6e95f97
Peter Nahm <hwnahm@gmail.com>
2021-07-05 18:04:36 +0900

    fix: change APR calculation parameters

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const BSC_BLOCK_TIME = 3
+export const BSC_BLOCK_TIME = 15   //  TODO : Mainnet 블록타임으로 교체해야 함.
 
 // TAL_PER_BLOCK details
 // 40 TAL is minted per block
@@ -16,7 +16,7 @@ export const BSC_BLOCK_TIME = 3
 // TAL_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // TAL/Block in src/views/Home/components/CakeStats.tsx = 20 (40 - Amount sent to burn pool)
 
-export const TAL_PER_BLOCK = new BigNumber(40)
+export const TAL_PER_BLOCK = new BigNumber(2.7)  // TODO: 스마트컨드랙 내 데이터와 일치 ?
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const TAL_PER_YEAR = TAL_PER_BLOCK.times(BLOCKS_PER_YEAR)
 // export const BASE_URL = 'https://pancakeswap.finance'
@@ -31,5 +31,5 @@ export const BASE_BSC_SCAN_URL = 'https://ropsten.etherscan.io'
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 1
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
-export const DEFAULT_GAS_LIMIT = 200000
+export const DEFAULT_GAS_LIMIT = 3000000
 export const DEFAULT_GAS_PRICE = 5
