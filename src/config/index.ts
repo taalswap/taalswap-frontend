b8e686611d3cb18214f5bbb7744d6d65087c3f57
Peter Nahm <hwnahm@gmail.com>
2021-07-07 01:58:59 +0900

    taalswap mainnet release

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const BSC_BLOCK_TIME = 15   //  TODO : Mainnet 블록타임으로 교체해야 함.
+export const BSC_BLOCK_TIME = 15
 
 // TAL_PER_BLOCK details
 // 40 TAL is minted per block
@@ -16,7 +16,7 @@ export const BSC_BLOCK_TIME = 15   //  TODO : Mainnet 블록타임으로 교체�
 // TAL_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // TAL/Block in src/views/Home/components/CakeStats.tsx = 20 (40 - Amount sent to burn pool)
 
-export const TAL_PER_BLOCK = new BigNumber(40)  // TODO: 스마트컨드랙 내 데이터와 일치 ?
+export const TAL_PER_BLOCK = new BigNumber(40)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const TAL_PER_YEAR = TAL_PER_BLOCK.times(BLOCKS_PER_YEAR)
 // export const BASE_URL = 'https://pancakeswap.finance'
@@ -27,7 +27,7 @@ export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/liquidity`
 // export const BASE_BSC_SCAN_URL = 'https://bscscan.com'
 // export const BASE_BSC_SCAN_URL = 'https://testnet.bscscan.com'
-export const BASE_BSC_SCAN_URL = 'https://ropsten.etherscan.io'
+export const BASE_BSC_SCAN_URL = 'https://etherscan.io'
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 1
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
--- src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/GetLpModal.tsx
+++ src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/GetLpModal.tsx
@@ -26,7 +26,7 @@ const GetLpModal: React.FC<Partial<Props>> = ({ currency, onDismiss }) => {
         <Button
           as={Link}
           external
-          href={`${BASE_ADD_LIQUIDITY_URL}/ETH/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82`}
+          href={`${BASE_ADD_LIQUIDITY_URL}/ETH/0x90a4a420732907b3c38b11058f9aa02b3f4121df`}
           endIcon={<OpenNewIcon color="white" />}
           minWidth="100%" // Bypass the width="fit-content" on Links
         >
