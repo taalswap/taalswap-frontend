28b720305dbd927b27cf70f0c183203b0fdb56ea
helloljho <helloljho@gmail.com>
2021-07-20 16:56:00 +0900

    add all pairs key symbol

--- src/pages/LandingPageView/AllPairs.tsx
+++ src/pages/LandingPageView/AllPairs.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
+import { ethers } from 'ethers'
 import { Text, Link, Button, IconButton, SyncAltIcon, AddIcon } from 'taalswap-uikit'
 import { useTranslation } from 'contexts/Localization'
 import CardValue from 'views/Home/components/CardValue'
@@ -88,7 +89,39 @@ const LinkStyle = styled(Link)`
 const BTextStyle = styled.td`
   font-size: 12px;
   width: 100%;
+  //
 `
+const HigherLogo = styled.img`
+  z-index: 2;
+  background-color: white;
+  border-radius: 50%;
+
+  border: 1px solid gray;
+`
+
+const CoveredLogo = styled.img`
+  z-index: 1;
+  position: absolute;
+  background-color: white;
+  border-radius: 50%;
+  border: 1px solid gray;
+  margin-left: 15px;
+`
+
+const TokenWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+`
+
+function DoubleTokenLogo({ a0, a1, size = 24, margin = false }) {
+  return (
+    <TokenWrapper>
+      <HigherLogo src={a0} alt="test" />
+      <CoveredLogo src={a1} alt="test" />
+    </TokenWrapper>
+  )
+}
 
 const AllPairs = () => {
   const { t } = useTranslation()
@@ -98,6 +131,32 @@ const AllPairs = () => {
 
   const linkToURL = (url: string) => {
     window.location.href = url
+  }
+
+  const isAddress = (value) => {
+    try {
+      return ethers.utils.getAddress(value.toLowerCase())
+    } catch {
+      return false
+    }
+  }
+
+  const getTokenIconPath = (address) => {
+    // 토큰 icon address
+    let path
+    const tokenIcon = address.toLowerCase()
+    if (
+      tokenIcon === '0x90a4a420732907b3c38b11058f9aa02b3f4121df' ||
+      tokenIcon === '0x086b00cf35e8873636384cd2b424c39ae875a8a9'
+    ) {
+      path = `https://taalswap.info/images/coins/${address.toLowerCase()}.png`
+    } else {
+      path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
+        address,
+      )}/logo.png`
+    }
+
+    return path
   }
 
   const pairTableRow = () => {
@@ -128,6 +187,11 @@ const AllPairs = () => {
 
         const volumn24h = pair.previous24hVolumeUSD
 
+        const baseSymbolPath = getTokenIconPath(pair.base_address)
+        const quoteSymbolPath = getTokenIconPath(pair.quote_address)
+
+        console.log(baseSymbolPath)
+        console.log(quoteSymbolPath)
         // let prices = ''
         // if (pair.base_symbol === 'TSHP') {
         //   prices = `${process.env.REACT_APP_INTERFACE}/#/swap/${pair.quote_address}/${pair.base_address}`
@@ -147,6 +211,8 @@ const AllPairs = () => {
           prices,
           base_symbol,
           deposit,
+          baseSymbolPath,
+          quoteSymbolPath,
         }
 
         resultRow.push(temp)
@@ -214,7 +280,7 @@ const AllPairs = () => {
         <tbody>
           <tr>
             <TitleStyle style={{ width: '30%' }}>{t('Pair')}</TitleStyle>
-            <TitleStyle>{t('Price ($)')}</TitleStyle>
+            {/* <TitleStyle>{t('Price ($)')}</TitleStyle> */}
             <TitleStyle>{t('Volume (24H)')}</TitleStyle>
             <TitleIconStyle>{t('Swap')}</TitleIconStyle>
             <TitleIconStyle>{t('LP')}</TitleIconStyle>
@@ -223,8 +289,17 @@ const AllPairs = () => {
             .sort((pairA, pairB) => pairB.volumn24h - pairA.volumn24h)
             .map((pair) => (
               <tr key={pair.name}>
-                <TextStyle style={{ verticalAlign: 'middle' }}>{pair.name}</TextStyle>
                 <TextStyle style={{ verticalAlign: 'middle' }}>
+                  {/* <DoubleTokenLogo a0={pair.baseSymbolPath} a1={pair.quoteSymbolPath} size={16} /> */}
+                  <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+                    <TokenWrapper>
+                      <HigherLogo src={pair.baseSymbolPath} alt="test" width="24px" height="24px" />
+                      <CoveredLogo src={pair.quoteSymbolPath} alt="test" width="24px" height="24px" />
+                    </TokenWrapper>
+                    <div style={{ marginLeft: '20px' }}>{pair.name}</div>
+                  </div>
+                </TextStyle>
+                {/* <TextStyle style={{ verticalAlign: 'middle' }}>
                   <div style={{ display: 'flex', alignItems: 'center' }}>
                     {pair.price >= 1 ? (
                       <CardValue value={pair.price} decimals={2} fontSize="14px" />
@@ -232,7 +307,7 @@ const AllPairs = () => {
                       <CardValue value={pair.price} decimals={8} fontSize="14px" />
                     )}
                   </div>
-                </TextStyle>
+                </TextStyle> */}
                 <TextStyle style={{ verticalAlign: 'middle' }}>
                   <div style={{ display: 'flex', alignItems: 'center' }}>
                     <span style={{ marginRight: '5px' }}>$</span>
