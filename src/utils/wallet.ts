0931a45f5ee2b7e204821a4d3dd684fa29239043
Peter Nahm <hwnahm@gmail.com>
2021-07-22 16:07:31 +0900

    fix: remove BSC

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -17,10 +17,10 @@ export const setupNetwork = async () => {
         params: [
           {
             chainId: `0x${chainId.toString(16)}`,
-            chainName: 'Binance Smart Chain Mainnet',
+            chainName: 'Ethereum Mainnet',
             nativeCurrency: {
               name: 'ETH',
-              symbol: 'bnb',
+              symbol: 'eth',
               decimals: 18,
             },
             rpcUrls: nodes,
@@ -34,7 +34,7 @@ export const setupNetwork = async () => {
       return false
     }
   } else {
-    console.error("Can't setup the BSC network on metamask because window.ethereum is undefined")
+    console.error("Can't setup the ethereum mainnet on metamask because window.ethereum is undefined")
     return false
   }
 }
