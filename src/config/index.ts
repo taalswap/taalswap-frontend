1c7f78b8d85932cbfacceeda04b43dd598ade3ad
Peter Nahm <hwnahm@gmail.com>
2021-08-17 14:27:01 +0900

    fix: metamask rpc error "max fee per gas less than block base fee"

--- src/config/index.ts
+++ src/config/index.ts
@@ -31,5 +31,5 @@ export const BASE_BSC_SCAN_URL = 'https://etherscan.io'
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 1
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
-export const DEFAULT_GAS_LIMIT = 3000000
-export const DEFAULT_GAS_PRICE = 5
+export const DEFAULT_GAS_LIMIT = 50000000000
+export const DEFAULT_GAS_PRICE = 50
