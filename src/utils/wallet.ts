052952ad6376430edf87ace0ace34746c5d65927
Peter Nahm <hwnahm@gmail.com>
2021-09-06 21:29:19 +0900

    fix: wallet_addEthereumChain support more test net

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,7 +1,78 @@
 // Set of helper functions to facilitate wallet setup
 import { ChainId } from 'taalswap-sdk'
-import { BASE_BSC_SCAN_URL, SCAN_URL } from 'config'
+import { BASE_BSC_SCAN_URL, SCAN_URL, NETWORK_NAME } from 'config'
 import { nodes } from './getRpcUrl'
+
+const addNetwork = async (chainId: number) => {
+  const provider = (window as WindowChain).ethereum
+  if (provider && provider.request) {
+    try {
+      if (chainId === ChainId.MAINNET || chainId === ChainId.ROPSTEN || chainId === ChainId.RINKEBY) {
+        await provider.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: `0x${chainId.toString(16)}`,
+              chainName: NETWORK_NAME[chainId],
+              nativeCurrency: {
+                name: 'ETH',
+                symbol: 'ETH',
+                decimals: 18,
+              },
+              rpcUrls: [`${process.env.REACT_APP_NETWORK_URL}`],
+              blockExplorerUrls: [`${SCAN_URL[chainId]}/`],
+              chainType: "TaalSwap"
+            },
+          ],
+        });
+      } else if (chainId === ChainId.BAOBAB) {
+        await provider.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: `0x${chainId.toString(16)}`,
+              chainName: 'Klaytn Baobab',
+              nativeCurrency: {
+                name: 'klay',
+                symbol: 'KLAY',
+                decimals: 18,
+              },
+              rpcUrls: ['https://api.baobab.klaytn.net:8651'],
+              blockExplorerUrls: ['https://baobab.scope.klaytn.com/'],
+              chainType: "TaalSwap"
+            },
+          ],
+        });
+      } else if (chainId === ChainId.KLAYTN) {
+        await provider.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: `0x${chainId.toString(16)}`,
+              chainName: 'Klaytn Mainnet',
+              nativeCurrency: {
+                name: 'KLAY',
+                symbol: 'KLAY',
+                decimals: 18,
+              },
+              rpcUrls: ['https://klaytn.taalswap.info:8651'],
+              blockExplorerUrls: ['https://scope.klaytn.com/'],
+              chainType: "TaalSwap"
+            },
+          ],
+        });
+      }
+    } catch (addError) {
+      // handle "add" error
+      console.error(addError)
+      return false
+    }
+  } else {
+    console.error("Can't setup the ethereum mainnet on metamask because window.ethereum is undefined")
+    return false
+  }
+  return true
+}
 
 /**
  * Prompt the user to add BSC as a network on Metamask, or switch to BSC if the wallet is on a different network
@@ -11,6 +82,7 @@ export const setupNetwork = async (chainId: number) => {
   const provider = (window as WindowChain).ethereum
   if (provider) {
     try {
+      await addNetwork(chainId)   // Talken
       await provider.request({
         method: 'wallet_switchEthereumChain',
         params: [{ chainId: `0x${chainId.toString(16)}` }],
@@ -19,64 +91,7 @@ export const setupNetwork = async (chainId: number) => {
     } catch (error) {
       // This error code indicates that the chain has not been added to MetaMask.
       if (error.code === 4902) {
-        try {
-          if (chainId === ChainId.MAINNET) {
-            await provider.request({
-              method: 'wallet_addEthereumChain',
-              params: [
-                {
-                  chainId: `0x${chainId.toString(16)}`,
-                  chainName: 'Ethereum Mainnet',
-                  nativeCurrency: {
-                    name: 'ETH',
-                    symbol: 'ETH',
-                    decimals: 18,
-                  },
-                  rpcUrls: nodes,
-                  blockExplorerUrls: [`${BASE_BSC_SCAN_URL}/`],
-                },
-              ],
-            });
-          } else if (chainId === ChainId.BAOBAB) {
-            await provider.request({
-              method: 'wallet_addEthereumChain',
-              params: [
-                {
-                  chainId: `0x${chainId.toString(16)}`,
-                  chainName: 'Klaytn Baobab',
-                  nativeCurrency: {
-                    name: 'klay',
-                    symbol: 'KLAY',
-                    decimals: 18,
-                  },
-                  rpcUrls: ['https://api.baobab.klaytn.net:8651/'],
-                  blockExplorerUrls: ['https://baobab.scope.klaytn.com/'],
-                },
-              ],
-            });
-          } else if (chainId === ChainId.KLAYTN) {
-            await provider.request({
-              method: 'wallet_addEthereumChain',
-              params: [
-                {
-                  chainId: `0x${chainId.toString(16)}`,
-                  chainName: 'Klaytn Mainnet',
-                  nativeCurrency: {
-                    name: 'KLAY',
-                    symbol: 'KLAY',
-                    decimals: 18,
-                  },
-                  rpcUrls: ['https://klaytn.taalswap.info:8651'],
-                  blockExplorerUrls: ['https://scope.klaytn.com/'],
-                },
-              ],
-            });
-          }
-        } catch (addError) {
-          // handle "add" error
-          console.error(error)
-          return false
-        }
+        await addNetwork(chainId)
       } else if (error.code === 4001) {
         return false
       }
