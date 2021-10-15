fe2d7ccd07ec86ad4aa6cfb207e18694bcc8326a
hanabpg3 <84052764+hanabpg3@users.noreply.github.com>
2021-10-15 14:57:07 +0900

    farms 아이콘수정
    farms 아이콘수정
    - 리스트 디자인
    - 카드 디자인

--- src/views/Farms/components/FarmTable/Farm.tsx
+++ src/views/Farms/components/FarmTable/Farm.tsx
@@ -4,6 +4,8 @@ import { useFarmUser } from 'state/hooks'
 import { useTranslation } from 'contexts/Localization'
 import { Text, Image } from 'taalswap-uikit'
 import { getBalanceNumber } from 'utils/formatBalance'
+import CoinImg01 from '../../../../pages/LandingPageView/images/coin_eth_icon.svg'
+import CoinImg02 from '../../../../pages/LandingPageView/images/coin_taal_icon.svg'
 
 export interface FarmProps {
   label: string
@@ -11,21 +13,38 @@ export interface FarmProps {
   image: string
 }
 
+const IconImageBody = styled.div`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  > div:nth-child(1) {
+      z-index: 2;
+    }
+  > div:nth-child(2) {
+      margin-left: -15px;
+      z-index: 1;
+    }
+`;
+
 const IconImage = styled(Image)`
   width: 24px;
   height: 24px;
 
+  /*
   ${({ theme }) => theme.mediaQueries.sm} {
     width: 40px;
     height: 40px;
   }
+  */
 `
 
 const Container = styled.div`
   padding-left: 16px;
   display: flex;
   align-items: center;
-  max-width:140px;
+  max-width:atuo;
 
   ${({ theme }) => theme.mediaQueries.sm} {
   }
@@ -50,7 +69,11 @@ const Farm: React.FunctionComponent<FarmProps> = ({ image, label, pid }) => {
 
   return (
     <Container>
-      <IconImage src={`/images/farms/${image}.svg`} alt="icon" width={40} height={40} mr="8px" />
+      {/* <IconImage src={`/images/farms/${image}.svg`} alt="icon" width={40} height={40} mr="8px" /> */ }
+      <IconImageBody>
+        <IconImage src={CoinImg01} alt="icon" width={40} height={40} mr="8px" />
+        <IconImage src={CoinImg02} alt="icon" width={40} height={40} mr="8px" />
+      </IconImageBody>
       <div style={{width:'100%'}}>
         {handleRenderFarming()}
         <Text bold fontSize='14px'>{label}</Text>
