8960fcc37d0f8eee39b275ef1494598a4e9fcd83
helloljho <helloljho@gmail.com>
2022-01-05 14:12:16 +0900

    fix : remove lazy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React from 'react'
 import { Router, Redirect, Route, Switch } from 'react-router-dom'
 import { ResetCSS } from 'taalswap-uikit'
 import BigNumber from 'bignumber.js'
@@ -11,30 +11,21 @@ import AddLiquidity from 'views/AddLiquidity'
 import RemoveLiquidity from 'views/RemoveLiquidity'
 import GlobalStyle from './style/Global'
 import Menu from './components/Menu'
-import SuspenseWithChunkError from './components/SuspenseWithChunkError'
+// import SuspenseWithChunkError from './components/SuspenseWithChunkError'
 import ToastListener from './components/ToastListener'
-import PageLoader from './components/PageLoader'
+// import PageLoader from './components/PageLoader'
 import EasterEgg from './components/EasterEgg'
 import Pools from './views/Pools'
+import Farms from './views/Farms'
 import history from './routerHistory'
 import LandingPageView from './pages/LandingPageView'
+import NotFound from './views/NotFound'
 
 import { RedirectDuplicateTokenIds, RedirectOldAddLiquidityPathStructure } from './views/AddLiquidity/redirects'
 import { RedirectOldRemoveLiquidityPathStructure } from './views/RemoveLiquidity/redirects'
 
 // Route-based code splitting
 // Only pool is included in the main bundle because of it's the most visited page
-const Home = lazy(() => import('./views/Home'))
-const Farms = lazy(() => import('./views/Farms'))
-// const Lottery = lazy(() => import('./views/Lottery'))
-// const Ifos = lazy(() => import('./views/Ifos'))
-const NotFound = lazy(() => import('./views/NotFound'))
-// const Collectibles = lazy(() => import('./views/Collectibles'))
-// const Teams = lazy(() => import('./views/Teams'))
-// const Team = lazy(() => import('./views/Teams/Team'))
-// const Profile = lazy(() => import('./views/Profile'))
-// const TradingCompetition = lazy(() => import('./views/TradingCompetition'))
-// const Predictions = lazy(() => import('./views/Predictions'))
 
 // This config is required for number formatting
 BigNumber.config({
@@ -58,87 +49,43 @@ const App: React.FC = () => {
         <Switch>
           <Route exact strict path="/" component={LandingPageView} />
           <Menu>
-            <SuspenseWithChunkError fallback={<PageLoader />}>
-              <Switch>
-                <Route path="/home" exact>
-                  <Home />
-                </Route>
-                {process.env.REACT_APP_SITE_STOP === 'true' ? (
-                  <>
-                    <Route path="/farms" exact>
-                      <Redirect to="/" />
-                    </Route>
-                    <Route path="/staking" exact>
-                      <Redirect to="/" />
-                    </Route>
-                    <Route path="/farms/:isAdmin">
-                      <Farms />
-                    </Route>
-                    <Route path="/staking/:isAdmin">
-                      <Pools />
-                    </Route>
-                  </>
-                ) : (
-                  <>
-                    <Route exact strict path="/swap" component={Swap} />
-                    <Route exact strict path="/xswap" component={XSwap} />
-                    <Route exact strict path="/liquidity" component={Pool} />
-                    <Route exact path="/add" component={AddLiquidity} />
-                    <Route path="/farms">
-                      <Farms />
-                    </Route>
-                    <Route path="/staking">
-                      <Pools />
-                    </Route>
+            {/* <SuspenseWithChunkError fallback={<PageLoader />}> */}
+            <Switch>
+              {process.env.REACT_APP_SITE_STOP === 'true' ? (
+                <>
+                  <Route path="/farms" exact>
+                    <Redirect to="/" />
+                  </Route>
+                  <Route path="/staking" exact>
+                    <Redirect to="/" />
+                  </Route>
+                  <Route path="/farms/:isAdmin">
+                    <Farms />
+                  </Route>
+                  <Route path="/staking/:isAdmin">
+                    <Pools />
+                  </Route>
+                </>
+              ) : (
+                <>
+                  <Route exact strict path="/swap" component={Swap} />
+                  <Route exact strict path="/xswap" component={XSwap} />
+                  <Route exact strict path="/liquidity" component={Pool} />
+                  <Route exact path="/add" component={AddLiquidity} />
+                  <Route path="/farms" component={Farms} />
+                  <Route path="/staking" component={Pools} />
 
-                    {/* Redirection: These old routes are still used in the code base */}
-                    <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                    <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-                    <Route exact path="/add/:chainId/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-                    <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-                  </>
-                )}
+                  {/* Redirection: These old routes are still used in the code base */}
+                  <Route exact strict path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+                  <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                  <Route exact path="/add/:chainId/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                  <Route exact strict path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
+                </>
+              )}
 
-                {/* 사용하지 않는 페이지 라우팅 시작 */}
-
-                {/* <Route path="/lottery">
-                  <Lottery />
-                </Route>
-                <Route path="/ifo">
-                  <Ifos />
-                </Route>
-                <Route path="/collectibles">
-                  <Collectibles />
-                </Route>
-                <Route exact path="/teams">
-                  <Teams />
-                </Route>
-                <Route path="/teams/:id">
-                  <Team />
-                </Route>
-                <Route path="/profile">
-                  <Profile />
-                </Route>
-                <Route path="/competition">
-                  <TradingCompetition />
-                </Route>
-                <Route path="/prediction">
-                  <Predictions />
-                </Route>
-
-
-                <Route path="/syrup">
-                  <Redirect to="/staking" />
-                </Route>
-                <Route path="/nft">
-                  <Redirect to="/collectibles" />
-                </Route> */}
-
-                {/* 사용하지 않는 페이지 라우팅 끝 */}
-                {/* 404 */}
-                <Route component={NotFound} />
-              </Switch>
-            </SuspenseWithChunkError>
+              <Route component={NotFound} />
+            </Switch>
+            {/* </SuspenseWithChunkError> */}
           </Menu>
           <EasterEgg iterations={2} />
           <ToastListener />
